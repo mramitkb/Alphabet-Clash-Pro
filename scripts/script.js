eed9df601914b55e487fd79240b8671e0418399c
Amit Kumar Bhuiyan <mr.amitkb@gmail.com>
2024-09-20 23:29:14 +0600

    Hurray! Alphabet matched.

--- scripts/script.js
+++ scripts/script.js
@@ -5,21 +5,45 @@ function playNow() {
 }
 
 function continueGame() {
-    // 1. get random alphabet
-    const alphabetsString = 'abcdefghijklmnopqrstuvwxyz';
-    const alphabets = alphabetsString.split('');
+    // 1. generate a random alphabet
+    const alphabet = generateARandomAlphabet();
 
-    const getRandomNumber = Math.round(Math.random() * 25);
-    const alphabet = alphabets[getRandomNumber]
-    // 2. set random alphabet on the playground screen
+    // 2. display the random alphabet
     const currentAlphabet = document.getElementById('current-alphabet');
     currentAlphabet.innerText = alphabet;
-    // console.log(currentAlphabet.innerText, alphabet);
+    // 3. set BG color on the current alphabet
+    setBGColorById(alphabet);
+
+    // 4. keyboard event start
+}
+
+document.addEventListener('keyup', handleKeyboardPress);
+function handleKeyboardPress(event) {
+    // 1. which alphabet pressed by the player
+    const playerPressed = event.key;
+
+    // 2. displayed alphabet
+    const currentAlphabetText = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetText.innerText.toLowerCase();
+
+    // 3. conditions apply
+    if(playerPressed === currentAlphabet) {
+        removeBGColorById(currentAlphabet);
+        continueGame()
+        console.log('right');
+    }
+    else {
+        console.log('Wrong');
+    }
+    console.log(currentAlphabet, playerPressed);
 }
 
 /**
- * 1. hide home screen and show playground screen(reusable function)
+ * 1. hide home screen and show playground screen
  * 2. display a random alphabet on the screen
+ * 3. set bg color with matched alphabet
+ * 4. start keyboard event with keyup/keypress
+ * 5. 
  * 
  */
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -10,5 +10,24 @@ function showScreenById(elementId) {
 }
 // set bg color in current alphabet
 function setBGColorById(elementId) {
-    
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+// remove bg color from current alphabet when life goes blank
+function removeBGColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+// get current alphabet from playground screen
+function getCurrentTextById(elementId) {
+    const element = document.getElementById(elementId);
+    console.log(element.innerText);
+}
+function generateARandomAlphabet() {
+    // 1. get random alphabet
+    const alphabetsString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetsString.split('');
+    const getRandomNumber = Math.round(Math.random() * 25);
+    const alphabet = alphabets[getRandomNumber]
+    return alphabet;
 }
